663515dd7917510b7c5adc943d03e196ff6fad43
Ramprasad-R <ramprasad.info@gmail.com>
2020-04-09 18:59:10 +0200

    Vue Nuxtjs Practice

---
+++ pages/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About Me</h1>
+    <p>This is About me page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About me",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "About me"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Welcome to view the Jokes</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Checkout Jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
